a3950c7a618976d93ce3b6b3758f90e13b68b184
Robin F <robinfgame@outlook.fr>
2017-04-25 18:19:19 +0200

    Mise en forme du module avec un main qui appel la fct lecture

--- Lecture.c
+++ Lecture.c
@@ -10,8 +10,12 @@
 #define MAXBUF 1024
 instruction *code;
 
+//Prototypes des fonctions
 int lecture(char *cheminFichierCode, instruction *memoire);
+int stockage(instruction *memoire, resultat *adresses, char *oc, registre *op);
+int verification(instruction *memoire, instruction *instr);
 
+//Main
 int main(int argc, char *argv[])
 {
 	if(argc < 2)
@@ -25,6 +29,21 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/*Fonction lecture
+ * Renvoie 1 si la lecture aboutie, sinon 0.
+ * cheminFichierCode est le chemin jusqu'au fichier à lire.
+ * memoire est un tableau d'instruction qu'on initialise en lisant
+ * ligne par ligne le code fourni à la fonction.
+ * 
+ * On va lire le code ligne par ligne et vérifier que chaque ligne
+ * lue est correcte par rapport au jeu d'instruction en appellant
+ * la fonction vérification qui contient le tableau jeu d'instruction
+ * une foi la vérification effectué on enregistre grâce à la fonction
+ * stockage dans le tableau d'instruction memoire.
+ * 
+ * Si une erreur est détecté on envoie un message d'erreur et on vide le
+ * tableau memoire. 
+ */
 int lecture(char *cheminFichierCode, instruction *memoire){
 	int fs;
 	int res;
@@ -66,4 +85,28 @@ int lecture(char *cheminFichierCode, instruction *memoire){
 	
 	close(fs);
     return 1;
+}
+
+/* Fonction verification
+ * Renvoie 1 si la vérification est bonne, sinon 0.
+ * 
+ * 
+ * 
+ */ 
+int verification(instruction *memoire, instruction * instr){
+	
+	return 1;
+}
+
+/* Fonction stockage
+ * Renvoie 1 si l'instruction a bien été stockée, sinon 0
+ * 
+ * 
+ * 
+ * 
+ */ 
+
+int stockage(instruction *memoire, resultat *adresses, char *oc, registre *op){
+	
+	return 1;
 }
