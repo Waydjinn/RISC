58ba6e6b313632a1f197d3c94e7930f5c49409ec
Robin F <robinfgame@outlook.fr>
2017-04-25 18:06:16 +0200

    Mise en forme du module avec un main qui appel la fct lecture

--- Lecture.c
+++ Lecture.c
@@ -8,11 +8,24 @@
 #include "structures.h"
 
 #define MAXBUF 1024
+instruction *code;
 
-int lecture(char *cheminFichierCode, instruction memoire);
+int lecture(char *cheminFichierCode, instruction *memoire);
 
 int main(int argc, char *argv[])
 {
+	if(argc < 2)
+	{
+		printf("Erreur, chemin fichier non fourni en argument\n");
+	}
+	else
+	{
+		lecture(argv[1], code);
+	}
+    return 0;
+}
+
+int lecture(char *cheminFichierCode, instruction *memoire){
 	int fs;
 	int res;
 	int k = 0;
@@ -22,41 +35,35 @@ int main(int argc, char *argv[])
 	char opcode[MAXBUF];
 	char oprande[MAXBUF];
 
-	if(argc < 2)
+	
+	fs = open(cheminFichierCode, O_RDONLY);
+	if(fs == -1)
 	{
-		printf("Erreur, chemin fichier non fourni en argument\n");
+		printf("Le fichier est introuvable\n");
+		return 0;
 	}
-	else
+	while(res != 0)
 	{
-		fs = open(argv[1], O_RDONLY);
-		if(fs == -1)
+		tempCode = '1';
+		while(tempCode != ' ' && res != 0) //On recopie la ligne caractère par caracère
 		{
-			printf("Le fichier est introuvable\n");
-			exit(0);
+			res = read(fs, &tempCode, 1);
+			opcode[k] = tempCode;
+			k++;
 		}
-		while(res != 0)
+		tempParam = '1';
+		while(tempParam != '\n' && res != 0)
 		{
-			tempCode = '1';
-			while(tempCode != ' ' && res != 0) //On recopie la ligne caractère par caracère
-			{
-				res = read(fs, &tempCode, 1);
-				opcode[k] = tempCode;
-				k++;
-			}
-			tempParam = '1';
-			while(tempParam != '\n' && res != 0)
-			{
-				res = read(fs, &tempParam, 1);
-				oprande[l] = tempParam;
-				l++;
-			}
-			k = 0;
-			l = 0;
-			printf("opcode : %s \n", opcode);
-			printf("oprande : %s \n", oprande);
+			res = read(fs, &tempParam, 1);
+			oprande[l] = tempParam;
+			l++;
 		}
+		k = 0;
+		l = 0;
+		printf("opcode : %s \n", opcode);
+		printf("oprande : %s \n", oprande);
 	}
 	
 	close(fs);
-    return 0;
+    return 1;
 }
