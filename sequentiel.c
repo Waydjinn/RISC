da0958a02506d1045feb583e4fcbf7ab59286b0e
RobinFInf <robinfgame@outlook.fr>
2017-05-28 22:11:05 +0200

    Update sequentiel.c

--- sequentiel.c
+++ sequentiel.c
@@ -26,42 +26,42 @@ void Sequentiel(instruction * Instruction)
 			{
 				ADD(&tabreg[Instruction[cp].r1],&tabreg[Instruction[cp].r2],&tabreg[Instruction[cp].r3]);
 				cycle[0] +=1;
-				printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
+				//printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
 				cp++;
 			}
 		if(strcmp(Instruction[cp].OPcode, "MULT" ) == 0)
 			{
 				MULT(&tabreg[Instruction[cp].r1],&tabreg[Instruction[cp].r2],&tabreg[Instruction[cp].r3]);
 				cycle[0] +=4;
-				printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
+				//printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
 				cp++;
 			}
 		if(strcmp(Instruction[cp].OPcode, "LW" ) == 0)
 			{
 				LW(&tabreg[Instruction[cp].r2],Instruction[cp].res,&tabreg[Instruction[cp].r1]);
 				cycle[0] +=2;
-				printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
+				//printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
 				cp++;
 			}
 		if(strcmp(Instruction[cp].OPcode, "SW" ) == 0)
 			{
 				SW(&tabreg[Instruction[cp].r2],Instruction[cp].res,&tabreg[Instruction[cp].r1]);
 				cycle[0] +=2;
-				printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
+				//printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
 				cp++;
 			}
 		if(strcmp(Instruction[cp].OPcode, "SLT" ) == 0)
 			{
 				SLT(&tabreg[Instruction[cp].r1],&tabreg[Instruction[cp].r2],&tabreg[Instruction[cp].r3]);
 				cycle[0] +=1;
-				printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
+				//printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
 				cp++;
 			}
 		if(strcmp(Instruction[cp].OPcode, "BNEZ" ) == 0)
 			{
-				printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
+				//printf("cp = %d  et OPcode = %s \n",cp,Instruction[cp].OPcode);
 				cp += BNEZ(&tabreg[Instruction[cp].r1],Instruction[cp].jump);
-				printf("New cp = %d \n",cp);
+				//printf("New cp = %d \n",cp);
 				cycle[0]+=1;
 			} 
 
